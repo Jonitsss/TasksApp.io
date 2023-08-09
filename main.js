abbf78ee1f77150a29ad1730f8670a7cbbd1a3d1
Jonits <Jonathan0gab24@gmail.com>
2023-08-09 20:47:17 -0300

    Changes Js

--- main.js
+++ main.js
@@ -22,15 +22,8 @@ function toggleTextDecoration(btnNumber) {
     const alertBox = document.getElementById('customAlert');
     alertBox.style.display = 'none';
   }
-// Llamada a la función para cada botón y texto
-toggleTextDecoration(1);
-toggleTextDecoration(2);
-toggleTextDecoration(3);
-toggleTextDecoration(4);
-toggleTextDecoration(5);
-toggleTextDecoration(6);
-toggleTextDecoration(7);
-toggleTextDecoration(8);
-toggleTextDecoration(9);
-toggleTextDecoration(10);
 
+
+for (let i=0; i < 10; i++) {
+  toggleTextDecoration(i+1)
+}
